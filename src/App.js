d37482e043fbf697f467ee5659c919049c105226
Vitor Tomazzi <vitor.c.tomazzi@gmail.com>
2019-12-02 11:02:38 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: 'VitorCodes',
+			headerlinks: [
+				{ title: 'Home', path: '/' },
+				{ title: 'Projects', path: '/projects' },
+				{ title: 'About', path: '/about' },
+				{ title: 'Contact', path: '/contact' }
+			],
+			home: {
+				title: 'VitorCodes'
+			},
+			projects: {
+				title: 'Latest projects'
+			},
+			about: {
+				title: 'About me'
+			},
+			contact: {
+				title: "Let's connect"
+			}
+		};
+	}
 
-export default App;
+	render() {
+		return (
+			<Router>
+				<Container fluid className="p-0">
+					<Navbar expand="lg" className="border-bottom" style={{ backgroundColor: 'violet' }}>
+						<Navbar.Brand href="/home">VitorCodes</Navbar.Brand>
+						<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+						<Navbar.Collapse id="responsive-navbar-nav">
+							<Nav className="ml-auto">
+								<Nav.Link href="/projects">Projects</Nav.Link>
+								<Nav.Link href="/about">About</Nav.Link>
+								<Nav.Link href="/contact">Contact</Nav.Link>
+							</Nav>
+						</Navbar.Collapse>
+					</Navbar>
+				</Container>
+			</Router>
+		);
+	}
+}
